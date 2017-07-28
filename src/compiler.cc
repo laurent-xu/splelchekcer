e2ccd27d2e14b11ec8cdff34239de8206d330e86
Laurent XU <laurent.xu@lrde.epita.fr>
2017-07-28 19:05:46 +0200

    add compiler parser

--- src/compiler.cc
+++ src/compiler.cc
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <string>
-
-static void parse(const std::string&)
-{
-}
-
-static void write(const std::string&)
-{
-}
+#include <fstream>
+#include "trie.hh"
 
 void usage(const std::string& bin_name)
 {
@@ -21,11 +15,31 @@ int main(int argc, char* argv[])
 {
     if (argc != 3)
         usage(argv[0]);
+
+    auto input = std::string(argv[1]);
+    auto output = std::string(argv[2]);
+
+    auto in_stream = std::ifstream(input);
+    if (!in_stream.is_open())
     {
-        auto input = std::string(argv[1]);
-        auto output = std::string(argv[2]);
-        parse(input);
-        write(output);
+      std::cerr << "Couldn't open the file: " << input << std::endl;
+      return 1;
     }
+    auto trie = Trie();
+    trie.build(in_stream);
+    in_stream.close();
+
+    trie.to_dot(std::cout);
+
+    auto out_stream = std::ofstream(output);
+    if (!out_stream.is_open())
+    {
+      std::cerr << "Couldn't open the file: " << output << std::endl;
+      return 1;
+    }
+    //compact_trie ct;
+    //trie->serialize(out_stream);
+    out_stream.close();
+
     return 0;
 }
